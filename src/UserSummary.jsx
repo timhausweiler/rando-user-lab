44edcfcf8d8a911304432bb0a4e3e507d9e86cdb
timhausweiler <haustimp@gmail.com>
2022-01-03 11:27:21 +0100

    complete

--- src/UserSummary.jsx
+++ src/UserSummary.jsx
@@ -20,8 +20,19 @@ function UserSummary(props) {
   } else {
     return (
       <div>
+        <h1>Random user</h1>
+        <h2>This is a random user:</h2>
         <p>Name: {props.userData.name.first} {props.userData.name.last}</p>
         <p>Email: {props.userData.email}</p>
+
+
+        {showMore && 
+          <div><p>Street: {props.userData.location.street.name} {props.userData.location.street.number}</p>
+            <p>City: {props.userData.location.city}</p>
+            <p>State: {props.userData.location.state}</p>
+            <p>Username: {props.userData.login.username}</p></div>
+        }
+
         <button onClick={handleClick}>Show more</button>
       </div>);
   }
