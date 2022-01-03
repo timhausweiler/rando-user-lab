683c50458c3da7495edc6c84c04bc6f9d101c201
timhausweiler <haustimp@gmail.com>
2022-01-03 11:08:45 +0100

    bug fix

--- src/UserSummary.jsx
+++ src/UserSummary.jsx
@@ -1,18 +1,16 @@
 function UserSummary(props) {
-  const [showMore, setShowMore] = useState(false);
-
-  if (props.name === null) {
-    return (
+  if (props.userData === null) {
+    return ( 
       <div>
         There is no user data
       </div>
-    )
+    );
   } else {
     return (
       <div>
         <p>Name: {props.userData.name.first} {props.userData.name.last}</p>
         <p>Email: {props.userData.email}</p>
-      </div>)
+      </div>);
   }
 }
 
